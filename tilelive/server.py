b39994b7df430502cf46fd380238c9efdc62956e
Tom MacWright <macwright@gmail.com>
2010-07-06 18:47:22 -0400

    Sending field back

--- tilelive/server.py
+++ tilelive/server.py
@@ -95,6 +95,7 @@ class InspectValueHandler(tornado.web.RequestHandler):
                         'min': min(field_values, key=len),
                         'max': max(field_values, key=len),
                         'count': len(field_values),
+                        'field': field_name,
                         'values': sorted(list(set(field_values)))[start:end]
                     }
                 )
@@ -104,6 +105,7 @@ class InspectValueHandler(tornado.web.RequestHandler):
                         'min': min(field_values),
                         'max': max(field_values),
                         'count': len(field_values),
+                        'field': field_name,
                         'values': sorted(list(set(field_values)))[start:end]
                     }
                 )
