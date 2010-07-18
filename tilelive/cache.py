fcaa2c1de85ce98b06c6f18f1b0e796eac4d7b83
Tom MacWright <macwright@gmail.com>
2010-07-18 13:22:37 -0400

    Adding safe64 fn with tests

--- tilelive/cache.py
+++ tilelive/cache.py
@@ -45,6 +45,23 @@ class TLCache(object):
         self.directory = kwargs.get('directory', '')
         if not os.path.isdir(self.directory): os.mkdir(self.directory)
 
+    def safe64(self, url):
+        """ create filesystem-safe places for url-keyed data to be stored
+        
+        >>> t = TLCache(directory='/tmp/doctest')
+        >>> t.safe64('test')
+        ['dGVzdA==']
+        >>> t = TLCache(directory='/tmp/doctest')
+        >>> k = t.safe64('http://tilemill.s3.amazonaws.com/x86_64-1.9.part.86?AWSAccessKeyId=11YC4XXY9VV9X7K5F9G2&Expires=1378620506&Signature=DWXVChV4qYgkPyp5tjLD1Rc8XdIDWXVChV4qYgkPyp5tjLD1Rc8XdI%3DDWXVChV4qYgkPyp5tjLD1Rc8XdI%3D')
+        >>> k
+        ['aHR0cDovL3RpbGVtaWxsLnMzLmFtYXpvbmF3cy5jb20veDg2XzY0LTEuOS5wYXJ0Ljg2P0FXU0FjY2Vzc0tleUlkPTExWUM0WFhZOVZWOVg3SzVGOUcyJkV4cGlyZXM9MTM3ODYyMDUwNiZTaWduYXR1cmU9RFdYVkNoVjRxWWdrUHlwNXRqTEQxUmM4WGRJRFdYVkNoVjRxWWdrUHlwNXRqTEQxUmM4WGRJJTNERFdYVkNoVjRxWWdrUHlwNXR', 'qTEQxUmM4WGRJJTNE']
+        >>> len(k[0]) == 255
+        True
+        """
+        chunks = lambda l, n: [l[x: x+n] for x in xrange(0, len(l), n)]
+        url_64 = base64.urlsafe_b64encode(url)
+        return chunks(url_64, 255)
+
     def url2fs(self, url):
         """ encode a URL to be safe as a filename """
         uri, extension = os.path.splitext(url)
@@ -212,3 +229,7 @@ class MapCache(TLCache):
               [x for x in os.listdir(self.directory) if 
                 os.path.isfile(os.path.join(self.directory, x))]
           )
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
