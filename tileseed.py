d7cd2226e5e8d94900f1cab51a8d5c39be9b142b
Tom MacWright <macwright@gmail.com>
2010-09-15 11:05:58 -0400

    Updating tileseed to hit new servers

--- tileseed.py
+++ tileseed.py
@@ -71,12 +71,12 @@ class RenderThread:
         """ test a transfer by loading a file but not downloading it to anywhere """
         pts = urlparse(path)
         conn = httplib.HTTPConnection(pts.hostname, pts.port)
-        start = time.time()  
+        start = time.time()
         conn.request('GET', pts.path)
         request_time = time.time()
         resp = conn.getresponse()
         response_time = time.time()
-        conn.close()     
+        conn.close()
         print "%s took %.5f" % (path, (response_time - start))
 
     def loop(self):
@@ -92,8 +92,10 @@ class RenderThread:
             self.timed_transfer(tile_uri)
             self.q.task_done()
 
-def render_tiles(bbox, minZoom, maxZoom, mapfile, url):
-    print "render_tiles(",bbox, mapfile, url, ")"
+def render_tiles(bbox, minZoom, maxZoom, options):
+    mapfile = options.mapfile
+    url = options.url
+    print "render_tiles(",bbox, options.mapfile, options.url, ")"
 
     # Launch rendering threads
     queue = Queue(32)
@@ -115,24 +117,25 @@ def render_tiles(bbox, minZoom, maxZoom, mapfile, url):
         px0 = gprj.fromLLtoPixel(ll0,z)
         px1 = gprj.fromLLtoPixel(ll1,z)
 
-        # check if we have directories in place
         for x in range(int(px0[0]/256.0),int(px1[0]/256.0)+1):
             # Validate x co-ordinate
             if (x < 0) or (x >= 2**z):
                 continue
-            # check if we have directories in place
             for y in range(int(px0[1]/256.0),int(px1[1]/256.0)+1):
                 # Validate x co-ordinate
                 if (y < 0) or (y >= 2**z):
                     continue
+                if True:
+                    y = (2**z-1) - y
                 tile_uri = "%s/%s/%s/%d/%d/%d.png" % (
                         url.rstrip('/'),
-                        'tile',
+                        'tms', # TODO: make customizable
                         urlsafe_b64encode(options.mapfile),
                         z,
                         x,
                         y)
                 # Submit tile to be rendered into the queue
+                print "requesting %s" % tile_uri
                 t = (tile_uri, x, y, z)
                 queue.put(t)
 
