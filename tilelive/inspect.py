98366c089302350c658460d47376ceed88c7825e
Tom MacWright <macwright@gmail.com>
2010-09-25 12:47:01 +0100

    Fixing much-neglected inspect functionality

--- tilelive/inspect.py
+++ tilelive/inspect.py
@@ -2,6 +2,11 @@
 
 import tornado
 from server import TileLive
+import safe64, cache, tempfile, os, logging
+from tornado.escape import json_encode, json_decode
+from osgeo import ogr
+
+# TODO: tempfile should not be required
 
 """
   Data type inspection machinery. Could be sensitive; don't enable this
@@ -12,8 +17,8 @@ class InspectValueHandler(tornado.web.RequestHandler, TileLive):
     """ sample data from each datasource referenced by a mapfile """
     @tornado.web.asynchronous
     def get(self, mapfile_64, layer_id_64, field_name_64):
-        self.layer_id   = base64.urlsafe_b64decode(layer_id_64)
-        self.field_name = base64.urlsafe_b64decode(field_name_64)
+        self.layer_id   = safe64.decode(layer_id_64)
+        self.field_name = safe64.decode(field_name_64)
         self.application._map_cache.get(mapfile_64, self, self.async_callback(self.async_get))
 
     def async_get(self, mapnik_map):
@@ -23,19 +28,19 @@ class InspectValueHandler(tornado.web.RequestHandler, TileLive):
             field_values = [dict(f.properties).get(self.field_name)
                 for f in layer.datasource.all_features()]
 
-            start = 0 + int(self.get_argument('start', 0))
-            end = int(self.get_argument('limit', 30)) + \
-                int(self.get_argument('start', 0))
+            # start = 0 + int(self.get_argument('start', 0))
+            # end = int(self.get_argument('limit', 30)) + \
+            #     int(self.get_argument('start', 0))
 
-            stringlen = {'key': len} if isinstance(field_values[0], basestring) else {}
+            # stringlen = {'key': len} if isinstance(field_values[0], basestring) else {}
 
-            json = json_encode({
-                'min': min(field_values, **stringlen),
-                'max': max(field_values, **stringlen),
-                'count': len(set(field_values)),
-                'field': self.field_name,
-                'values': sorted(list(set(field_values)))[start:end]
-            })
+            # json = json_encode({
+            #     'min': min(field_values, **stringlen),
+            #     'max': max(field_values, **stringlen),
+            #     'count': len(set(field_values)),
+            #     'field': self.field_name,
+            #     'values': sorted(list(set(field_values)))[start:end]
+            # })
             self.jsonp(json, self.get_argument('jsoncallback', None))
         except IndexError:
             self.write(json_encode({'error': 'Layer not found'}))
@@ -47,7 +52,7 @@ class InspectLayerHandler(tornado.web.RequestHandler, TileLive):
     """ fields and field types of each datasource referenced by a mapfile """
     @tornado.web.asynchronous
     def get(self, mapfile_64, layer_id_64):
-        self.layer_id = base64.urlsafe_b64decode(layer_id_64)
+        self.layer_id = safe64.decode(layer_id_64)
         self.application._map_cache.get(mapfile_64, self, self.async_callback(self.async_get))
 
     def async_get(self, mapnik_map):
@@ -79,7 +84,7 @@ class InspectDataHandler(tornado.web.RequestHandler, TileLive):
     """ fields and field types of each datasource referenced by a mapfile """
     @tornado.web.asynchronous
     def get(self, data_url_64):
-        self.get_url = base64.urlsafe_b64decode(data_url_64)
+        self.get_url = safe64.decode(data_url_64)
         self.precache = cache.PreCache(directory=tempfile.gettempdir(), request_handler = self)
         self.precache.add(self.get_url)
         self.precache.execute(self.async_callback(self.async_get))
@@ -95,6 +100,7 @@ class InspectDataHandler(tornado.web.RequestHandler, TileLive):
     def shapefile_projection(self, shapefile_path):
         """ given a path to a shapefile, get the proj4 string """
         # TODO: remove OGR dependency
+        logging.info(shapefile_path)
         shapefile = ogr.Open(shapefile_path)
         if shapefile is not None:
             layer = shapefile.GetLayer(0).GetSpatialRef()
@@ -102,20 +108,44 @@ class InspectDataHandler(tornado.web.RequestHandler, TileLive):
                 return layer.ExportToProj4()
         return False
 
+class InspectFieldHandler(tornado.web.RequestHandler, TileLive):
+    """ fields and field types of each datasource referenced by a mapfile """
+    @tornado.web.asynchronous
+    def get(self, mapfile_64):
+        self.application._map_cache.get(mapfile_64, self, self.async_callback(self.async_get))
+
+    def async_get(self, mapnik_map):
+        json = json_encode(dict([
+            (layer.datasource.params().as_dict().get('id', layer.name),
+              {
+                'fields': dict(zip(layer.datasource.fields(),
+                [field.__name__ for field in layer.datasource.field_types()])),
+                'extent': self.layer_envelope(layer)
+              }
+            ) for layer in mapnik_map.layers]))
+        self.jsonp(json, self.get_argument('jsoncallback', None))
+        self.finish()
+
+    def layer_envelope(self, layer):
+        """ given a layer object, return an envelope of it in merc """
+        e = layer.envelope()
+        return [e.minx, e.miny, e.maxx, e.maxy]
+
 class InspectDataHandler(tornado.web.RequestHandler, TileLive):
     """ fields and field types of each datasource referenced by a mapfile """
     @tornado.web.asynchronous
     def get(self, data_url_64):
-        self.get_url = base64.urlsafe_b64decode(data_url_64)
-        self.precache = cache.PreCache(directory=tempfile.gettempdir(), request_handler = self)
+        self.get_url = safe64.decode(data_url_64)
+        self.precache = cache.PreCache(directory=tempfile.gettempdir(), request_handler=self)
         self.precache.add(self.get_url)
         self.precache.execute(self.async_callback(self.async_get))
-        return "Reimplementing"
 
     def async_get(self):
+        shapefile_dir = os.path.join(self.precache.directory, safe64.dir(self.get_url))
+        shapefile_path = [f for f in os.listdir(shapefile_dir) if f.endswith('shp')][0]
         self.jsonp({
           'data_url': self.get_url,
-          'srs': self.shapefile_projection(shapefile_path + '.shp')
+          'srs': self.shapefile_projection(os.path.join(shapefile_dir, shapefile_path))
         }, self.get_argument('jsoncallback', None))
         self.finish()
 
@@ -130,7 +160,8 @@ class InspectDataHandler(tornado.web.RequestHandler, TileLive):
         return False
 
 # Provide handlers to server
-handlers = [(r"/([^/]+)/fields.json", InspectFieldHandler),
+handlers = [
+  (r"/([^/]+)/fields.json", InspectFieldHandler),
   (r"/([^/]+)/data.json", InspectDataHandler),
   (r"/([^/]+)/([^/]+)/layer.json", InspectLayerHandler),
   (r"/([^/]+)/([^/]+)/([^/]+)/values.json", InspectValueHandler)]
